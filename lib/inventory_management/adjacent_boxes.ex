69056fb96cb5e481027b414039500ff79837ed31
Paul Byrne <paulgregorybyrne@gmail.com>
2018-12-02 20:18:23 +0100

    Refactor AdjacentBoxes

--- lib/inventory_management/adjacent_boxes.ex
+++ lib/inventory_management/adjacent_boxes.ex
@@ -6,25 +6,25 @@ defmodule InventoryManagement.AdjacentBoxes do
     |> Enum.map(&String.trim/1)
     |> Enum.map(&String.to_charlist/1)
     |> adjacent_boxes
-    |> common
+    |> box_intersection
     |> List.to_string()
   end
 
-  def adjacent_boxes(boxes) do
-    Enum.reduce_while(boxes, 0, fn box1, acc ->
-      if Enum.any?(boxes, fn box2 -> adjacent?(box1, box2) end) do
-        {:halt, {box1, Enum.find(boxes, fn box2 -> adjacent?(box1, box2) end)}}
+  defp adjacent_boxes(boxes) do
+    Enum.reduce_while(boxes, 0, fn box1, _ ->
+      if box2 = Enum.find(boxes, &adjacent?(box1, &1)) do
+        {:halt, {box1, box2}}
       else
-        {:cont, 0}
+        {:cont, nil}
       end
     end)
   end
 
-  def adjacent?(box1, box2) do
+  defp adjacent?(box1, box2) do
     Hamming.distance(box1, box2) == {:ok, 1}
   end
 
-  def common({box1, box2}) do
+  defp box_intersection({box1, box2}) do
     box1
     |> Enum.with_index()
     |> Enum.filter(fn {el, index} -> Enum.at(box2, index) == el end)
