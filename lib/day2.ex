77f8fe575c4eb3520b7d9549ee94c66cad2913a9
Paul Byrne <paulgregorybyrne@gmail.com>
2018-12-02 20:47:35 +0100

    Tidy code

--- lib/day2.ex
+++ lib/day2.ex
@@ -3,32 +3,31 @@ https://adventofcode.com/2018/day/2
 """
 
 defmodule Day2 do
-  alias InventoryManagement.Checksum
-  alias InventoryManagement.AdjacentBoxes
+  alias InventoryManagement.{AdjacentBoxes, Checksum}
+
+  def part1 do
+    read_in
+    |> checksum()
+  end
+
+  def part2 do
+    read_in
+    |> common_chars()
+  end
+
+  defp read_in do
+    {:ok, dir} = File.cwd()
+
+    Path.join([dir, "inputs", "input2.txt"])
+    |> File.stream!()
+    |> Enum.map(& &1)
+  end
 
   def checksum(input) do
     Checksum.calculate(input)
   end
 
-  def get_checksum_from_input do
-    {:ok, dir} = File.cwd()
-
-    Path.join([dir, "inputs", "input2.txt"])
-    |> File.stream!()
-    |> Enum.map(& &1)
-    |> checksum()
-  end
-
   def common_chars(input) do
     AdjacentBoxes.common_chars(input)
-  end
-
-  def get_common_chars_from_input do
-    {:ok, dir} = File.cwd()
-
-    Path.join([dir, "inputs", "input2.txt"])
-    |> File.stream!()
-    |> Enum.map(& &1)
-    |> common_chars()
   end
 end
